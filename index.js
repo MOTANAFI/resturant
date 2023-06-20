70853e972bf6090258dbba41c5beef202cddea7f
LONEWOLF143 <twaha2030@gmail.com>
2023-06-20 11:50:57 +0300

    added intersection obsever reveal section

--- index.js
+++ index.js
@@ -22,3 +22,24 @@ const options = {
 const headerObserver = new IntersectionObserver(stickyNav, options);
 
 headerObserver.observe(section1);
+
+
+// reveal sections
+const allSections = document.querySelectorAll('.section')
+const revealSection = function (entries, observer) {
+  const [entry] = entries
+  if(!entry.isIntersecting) return
+  entry.target.classList.remove('section--hidden')
+  observer.unobserve(entry.target)
+}
+
+const sectionObserver = new IntersectionObserver(revealSection,
+  {
+    root:null,
+    threshold:0.15
+  })
+
+  allSections.forEach(section => {
+    sectionObserver.observe(section);
+    section.classList.add("section--hidden")
+  })
